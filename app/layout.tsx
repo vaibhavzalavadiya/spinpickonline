c942ff47fdc519968467d44cdf06bb6cbbd9c2b8
vaibhavzalavadiya <vaibhavzalavadiya@ganil.com>
2026-02-21 09:43:13 +0530

    adsense add 21-2-26

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,12 @@ export default function RootLayout({
       <head>
         <SchemaScript schema={organizationSchema} />
         <SchemaScript schema={websiteSchema} />
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9187153323911677"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
       </head>
       <body
         className={`${inter.variable} ${spaceGrotesk.variable} antialiased`}
