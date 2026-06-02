75eb1dd705af7d417aceb680e9cb987f6636ce1f
vaibhavzalavadiya <vaibhavzalavadiya2@gmail.com>
2026-06-02 20:13:03 +0530

    tag manager code add  02-06-26

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N9XW98MX');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <SchemaScript schema={organizationSchema} />
         <SchemaScript schema={websiteSchema} />
         <link rel="manifest" href="/manifest.json" />
@@ -47,6 +58,16 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${spaceGrotesk.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N9XW98MX"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Header />
         <main className="min-h-screen">{children}</main>
         <Footer />
